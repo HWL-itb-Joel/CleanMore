27425a028b67fe39848aa611252aa09e2fdff1bf
jowee <joel.aguirre.7e7@itb.cat>
2025-05-16 22:43:19 +0200

    Bug Fix Animaciones 3P

--- Assets/_Scripts/NetworkStuff.cs
+++ Assets/_Scripts/NetworkStuff.cs
@@ -5,21 +5,23 @@ using Mirror;
 
 public class NetworkStuff : NetworkBehaviour
 {
-    [SerializeField] private GameObject FPSCam = null, TpMeshArms = null, TPFullEmployee, meshHead;
+    [SerializeField] private GameObject FPSCam = null, TpMeshArms = null, TPFullEmployee, EmployeeOnline, meshHead;
 
     void Start()
     {
         if (isLocalPlayer)
         {
             FPSCam.SetActive(true);
-            TPFullEmployee.SetActive(false);
+            TPFullEmployee.SetActive(true);
+            EmployeeOnline.SetActive(false);
             meshHead.GetComponent<SkinnedMeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
             TpMeshArms.GetComponent<SkinnedMeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
         }
         else
         {
             FPSCam.SetActive(false);
-            TPFullEmployee.SetActive(true);
+            TPFullEmployee.SetActive(false);
+            EmployeeOnline.SetActive(true);
             meshHead.GetComponent<SkinnedMeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
             TpMeshArms.GetComponent<SkinnedMeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
         }
